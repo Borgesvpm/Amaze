332669daefcca4e6e6b152a93c0e2d0bd56831af
Vinicius <44777359+Borgesvpm@users.noreply.github.com>
2021-02-18 15:35:39 +0100

    Add files via upload

--- Test_new_scripts/weight_plot.py
+++ Test_new_scripts/weight_plot.py
@@ -1,16 +1,23 @@
 import pandas as pd
-from datetime import datetime   
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 df = pd.read_csv('0220082200D5DF_weight.csv')
-print (df)
+print(df)
 
-x = df['Time']
+df.Date_and_Time = pd.to_datetime(df.Date_and_Time)
+
+x = df['Date_and_Time']
 y = df['Weight']
 
+df.info()
+
+#grp_date = df.groupby("Date")
+#weights_by_date = pd.DataFrame(grp_date.size(), columns=['num_sessions'])
+#print(weights_by_date)
+
 # plot
-plt.plot(x,y)
+plt.plot(x,y,linestyle="",marker="o")
 # beautify the x-labels
 plt.gcf().autofmt_xdate()
 
