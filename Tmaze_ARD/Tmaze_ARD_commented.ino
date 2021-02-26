e1da241fb7d09e31de29d882572cd99cb59686ad
MaheshKarnani <36512140+MaheshKarnani@users.noreply.github.com>
2021-02-26 19:13:38 +0100

    Add files via upload
    final commented 26.02.21

--- Tmaze_ARD/Tmaze_ARD_commented.ino
+++ Tmaze_ARD/Tmaze_ARD_commented.ino
@@ -14,8 +14,8 @@ long interval = 60000; //CHANGE THIS TO CHANGE POD TIMING
 const int pResistor1 = A0; // BB1
 const int pResistor2 = A1; // BB2
 const int pResistor3 = A2; //BB3 choice running wheel
-const int pResistor4 = A3; // BB4
-const int pResistor5 = A4; //BB5 choice food pod
+const int pResistor4 = A3; // BB4choice food pod
+const int pResistor5 = A4; //BB5 
 
 const int servoPin1 = 4;      // control servos 1-3
 const int servoPin2 = 5;
@@ -29,8 +29,9 @@ const int ard_pi_3 = 10;  // reports MODE=1 to Pi
 const int ard_pi_4 = 11;  // reports food pod entry to Pi
 const int ard_pi_5 = 2;  // reports running wheel pod entry to Pi
 
-const int CLOSE_DOOR1 = 135;      // Angle of 135 degrees -> door is closed
-const int OPEN_DOOR1 = 30;     // Angle of 20 degrees -> door is opened
+const int CLOSE_DOOR1 = 135;// Angle of 135 degrees -> door is closed
+const int OPEN_DOOR1 = 40;     // Angle of 20 degrees -> door is opened
+const int HELP_DOOR1 = 50;     //help on close so it does not get stuck
 const int CLOSE_DOOR2 = 155;      // Angle of 155 degrees -> door is closed
 const int OPEN_DOOR2 = 20;        // 20
 const int CLOSE_DOOR3 = 159;      // Angle of 157 degrees -> middle passage is closed
@@ -39,6 +40,7 @@ const int RELEASE_WHEEL = 100;      // Angle of 100 degrees -> WHEEL is free
 const int BRAKE_WHEEL = 20;  //20 CLAMPED
 const int SHOW_FOOD = 170;      // Angle of 120 degrees -> FOOD IN CENTER
 const int HIDE_FOOD = 123;  //175 HIDDEN
+const int FOOD_SPEED = 20; //delay for moving 1deg; higher is slower
 
 const int pi_ard_1 = 12;      // receive RFID command from Pi 
 const int pi_ard_2 = 13;       // receive weight decision from Pi
@@ -88,14 +90,13 @@ void setup(){
 
 
 servo1.attach(servoPin1);
-    for (pos = CLOSE_DOOR1; pos >= OPEN_DOOR1; pos -= 1) { // open
-    servo1.write(pos);           
-    delay(8);                    
-  }
+  servo1.write(OPEN_DOOR1);
   delay(1000);
-    for (pos = CLOSE_DOOR1; pos >= OPEN_DOOR1; pos -= 1) { // open
-    servo1.write(pos);           
-    delay(7);   
+    servo1.write(CLOSE_DOOR1); 
+    delay(100);
+    servo1.write(HELP_DOOR1); //unstick door1
+    delay(100);
+    servo1.write(CLOSE_DOOR1);//close
   delay(1000);
 servo2.attach(servoPin2);
   servo2.write(OPEN_DOOR2);
@@ -121,12 +122,12 @@ delay(1000);
     servo5.attach(servoPin5);  //for loops for slow servo movement
   for (pos5 = HIDE_FOOD; pos5 <= SHOW_FOOD; pos5 += 1) { // show food
     servo5.write(pos5);              
-    delay(30);                  
+    delay(FOOD_SPEED);                  
   }
   delay(1000);
   for (pos5 = SHOW_FOOD; pos5 >= HIDE_FOOD; pos5 -= 1) { // hide food
     servo5.write(pos5);
-    delay(30);
+    delay(FOOD_SPEED);
   }
   
   digitalWrite(ard_pi_1, LOW);//communication to Pi
@@ -186,6 +187,10 @@ if (START == 1){
     Serial.println("animal in scale"); 
     servo1.write(CLOSE_DOOR1); //trap animal in scale
     delay(100);
+    servo1.write(HELP_DOOR1); //unstick door1
+    delay(100);
+    servo1.write(CLOSE_DOOR1);//close
+    delay(100);
     digitalWrite(ard_pi_1, HIGH);//pulse to Pi to start reading scale and go MODE 2.
     delay(100);
     digitalWrite(ard_pi_1, LOW);//end pulse to Pi.
@@ -218,62 +223,61 @@ if (MODE==3){ //Animal in T-maze deciding: food pod, running wheel or go home.
   // maze_mode=1,2,3 for (1=open rewards, 2=countdown timer, 3=close rewards)
   
   //maze start scenario
-  photo_value3 = analogRead(pResistor3);
   photo_value5 = analogRead(pResistor5);
-  if (flag == 0){ //coming from start side
-  if (photo_value3 < 0.7*INIT_READ3){   //entered run wheel pod
-    flag = 1; maze_mode=1;time_flag=1;tic=millis(); //set flags and start timer
+  photo_value4 = analogRead(pResistor4);
+  if (flag == 0){
+  if (photo_value5 < 0.7*INIT_READ5){   //in run wheel pod
+    flag = 1; maze_mode=1;time_flag=1;tic=millis();
     Serial.println("enter runwheel pod"); 
     digitalWrite(ard_pi_5, HIGH);//pulse to Pi to say runwheel pod entry.
-    for (pos = OPEN_DOOR3; pos <= CLOSE_DOOR3; pos += 1) { // close door 3 gently
+    for (pos = OPEN_DOOR3; pos <= CLOSE_DOOR3; pos += 1) { // close
     servo3.write(pos);     
     delay(8);              
   }
 
-    servo4.write(RELEASE_WHEEL); //release running wheel so animal can run in pod
+    servo4.write(RELEASE_WHEEL);
   delay(10);
     digitalWrite(ard_pi_5, LOW);//end pulse to Pi.
   }
-    if (photo_value5 < 0.7*INIT_READ5){   //in food pod
-    flag = 1; maze_mode=1;time_flag=1;tic=millis(); //set flags and start timer
+    if (photo_value4 < 0.7*INIT_READ4){   //in food pod
+    flag = 1; maze_mode=1;time_flag=1;tic=millis();
     Serial.println("enter food pod"); 
     digitalWrite(ard_pi_4, HIGH);//pulse to Pi to say foodpod entry.
-    for (pos = OPEN_DOOR3; pos <= CLOSE_DOOR3; pos += 1) { // close door 3 gently
+    for (pos = OPEN_DOOR3; pos <= CLOSE_DOOR3; pos += 1) { // close
     servo3.write(pos);     
     delay(8);              
   }
-      servo4.write(RELEASE_WHEEL); //release running wheel for consistency even when animal entered food pod.
+      servo4.write(RELEASE_WHEEL);
   delay(10);
     digitalWrite(ard_pi_4, LOW);//end pulse to Pi.
   }}
 
-if (flag == 1){ //
+if (flag == 1){
   if (maze_mode==1){//non-blocking loop to move food to center without delays to code since animal could go elsewhere.
-    if (time_flag==1){tic2=millis();time_flag=0;}
+    if (time_flag==1){tic2=millis();time_flag=0;} //time_flag allows tic2 to be updated only once at start
     if (pos5 < SHOW_FOOD){
     duration2=millis()-tic2;
-if (duration2>30){
+if (duration2>FOOD_SPEED){
 servo5.write(pos5); 
-pos5=pos5+1;tic2=millis();
+pos5=pos5+1;tic2=millis(); //when FOOD_SPEED 20ms has passed, servo moves 1deg and tic2 updated
     }}
     }
     if (pos5 == SHOW_FOOD){
-      maze_mode=2; pos5=SHOW_FOOD-1;time_flag2=1;
+      maze_mode=2; pos5=SHOW_FOOD-1;time_flag2=1; //when servo at target, maze_mode changed, pos5 moved so this loop does not repeat, time_flag2 for next cycle
     }
-  if (maze_mode==2){
-    duration=millis()-tic;
-//    Serial.print("duration is "); 
-//    Serial.println(duration);
+  if (maze_mode==2){ //maze_mode for waiting for interval or BB2
+    duration=millis()-tic;//timer to remove food and brake run-wheel after interval
+
   }
-  if (duration>interval){
-    maze_mode=3;
-    if (time_flag2==1){tic=millis();time_flag=1;time_flag2=0;time_flag3=1;}
+  if (duration>interval){ //timer criterion
+    maze_mode=3; //maze_mode for removing food and braking run-wheel
+    if (time_flag2==1){tic=millis();time_flag=1;time_flag2=0;time_flag3=1;}//time_flag2 so tic, time_flag and time_flag3 only updated once at start
   servo4.write(BRAKE_WHEEL);
   //non-blocking for loop to move food away without delays to code since animal could be elsewhere.
-       if (time_flag==1){tic2=millis();time_flag=0;}
+       if (time_flag==1){tic2=millis();time_flag=0;} //update only once at start
     if (pos5 > HIDE_FOOD){
     duration2=millis()-tic2;
-if (duration2>30){
+if (duration2>FOOD_SPEED){ //when FOOD_SPEED (20ms) has passed, servo moves 1deg and tic2 updated
 tic2=millis();
 servo5.write(pos5); 
 pos5=pos5-1;
@@ -282,12 +286,12 @@ pos5=pos5-1;
   
     if (flag == 0){ //animal has passed to start side!
     duration=0;maze_mode=3;
-    if (time_flag3==1){tic=millis();time_flag3=0;}
+    if (time_flag3==1){tic=millis();time_flag3=0;}//update only once at start
   servo4.write(BRAKE_WHEEL);
   //non-blocking for loop to move food away without delays to code since animal could be elsewhere.
-    if (pos5 > HIDE_FOOD){
-    duration=millis()-tic;
-if (duration>30){
+    if (pos5 > HIDE_FOOD){ 
+    duration=millis()-tic; //keep updating until position reached
+if (duration>FOOD_SPEED){ //when FOOD_SPEED (20ms) has passed, servo moves 1deg and tic2 updated
 tic=millis();
 servo5.write(pos5); 
 pos5=pos5-1;
@@ -295,12 +299,12 @@ pos5=pos5-1;
   }
   
   //maze new trial scenario
-  photo_value2 = analogRead(pResistor2);
+  photo_value2 = analogRead(pResistor2); //read BB2
   if (flag == 1){
-  if (photo_value2 < 0.8*INIT_READ2){   
-    flag=0;
+  if (photo_value2 < 0.8*INIT_READ2){   //BB2 tripped
+    flag=0; //flag out of this loop so that door3 only moved once
     Serial.println("trial"); 
-    digitalWrite(ard_pi_2, HIGH);//pulse to Pi new trial
+    digitalWrite(ard_pi_2, HIGH);//tell Pi new trial has started
     for (pos = CLOSE_DOOR3; pos >= OPEN_DOOR3; pos -= 1) { // open
     servo3.write(pos);           
     delay(8);                    
@@ -309,10 +313,10 @@ pos5=pos5-1;
   }}
 
   //maze exit scenario
-  photo_value1 = analogRead(pResistor1);
-  if (photo_value1 < 0.8*INIT_READ1){    
+  photo_value1 = analogRead(pResistor1); //read BB1
+  if (photo_value1 < 0.8*INIT_READ1){    //BB1 tripped
       Serial.println("animal wants out"); 
-      servo2.write(CLOSE_DOOR2);
+      servo2.write(CLOSE_DOOR2); 
       delay(100);
       MODE=4;
           Serial.print("MODE IS "); 
@@ -323,10 +327,14 @@ Serial.println(MODE);
    if (MODE==4){ 
      //going back to cage
      servo1.write(OPEN_DOOR1);
-if (photo_value4 < 0.8*INIT_READ4){    // values needs to be adjusted based on ambient light
-     servo1.write(CLOSE_DOOR1);
-     delay(100);
-     digitalWrite(ard_pi_3, HIGH);//pulse to Pi to sync to MODE 1.
+if (photo_value3 < 0.8*INIT_READ3){ //BB5 tripped
+    servo1.write(CLOSE_DOOR1); //trap animal in home cage
+    delay(100);
+    servo1.write(HELP_DOOR1); //unstick door1
+    delay(100);
+    servo1.write(CLOSE_DOOR1);//close
+    delay(100);
+     digitalWrite(ard_pi_3, HIGH);//tell Pi to sync to MODE 1.
      delay(100);
      digitalWrite(ard_pi_3, LOW);//end pulse to Pi.
         MODE=1;
