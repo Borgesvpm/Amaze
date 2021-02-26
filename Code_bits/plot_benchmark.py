e1222aea3dbf9b5dfa41b547acc6ffd5063aec00
Borgesvpm <44777359+Borgesvpm@users.noreply.github.com>
2021-02-26 13:44:53 +0100

    useful

--- Code_bits/plot_benchmark.py
+++ Code_bits/plot_benchmark.py
@@ -1,8 +1,48 @@
 from matplotlib import pyplot as plt
+from datetime import datetime
 
-x = [4,5,6]
-y = range(len(x))
-plt.scatter(x,y,color="#6c3376", linewidth=3)
+ys = []
+
+# start while loop
+start = datetime.now() #start of loop
+# your code here
+end = datetime.now() #end of loop
+
+datapoint = (end-start) 
+datapoint_float = datapoint.total_seconds()
+ys.append(datapoint_float)
+
+# Example measuring performance of the entire code
+
+ys = []
+
+start = datetime.now() #start of loop
+for i in range(1000):
+    print(i)
+end = datetime.now() #end of loop
+
+datapoint = (end-start) 
+datapoint_float = datapoint.total_seconds()
+ys.append(datapoint_float)
+
+# Example measuring performance of every loop
+
+ys = []
+
+
+for i in range(1000):
+    start = datetime.now() #start of loop
+    print(i)
+    end = datetime.now() #end of loop
+
+    datapoint = (end-start) 
+    datapoint_float = datapoint.total_seconds()
+    ys.append(datapoint_float)
+
+# plotting performance of every loop
+
+x = range(len(ys))
+plt.scatter(x,ys,color="#6c3376", linewidth=0.1)
 
 plt.show()
 plt.savefig("benchmark.png")
--- Code_bits/time_passed.py
+++
@@ -1,8 +0,0 @@
-import time
-
-oldtime = time.time()
-# check
-while True:
-    if time.time() - oldtime > 10:
-        print ("it's been 10 seconds")
-        break
