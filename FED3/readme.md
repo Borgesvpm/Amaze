bbfa2dfb58fffa114c29e19c37215929f4dd24c7
Borgesvpm <44777359+Borgesvpm@users.noreply.github.com>
2021-04-09 16:26:50 +0200

    FED

---
+++ FED3/readme.md
@@ -0,0 +1,21 @@
+## This is the FED3 library (beta)
+The goal of this library is to automate most of the housekeeping functions of [FED3](https://github.com/KravitzLabDevices/FED3) behind the scenes and let the user focus on writing the behavioral control. See the [Wiki](https://github.com/KravitzLabDevices/FED3_library/wiki) for documentation on how to use the library.   This library is in development, please report bugs using Issues. 
+
+## Installation instructions
+### 1. Install the FED3 library from the Arduino IDE <br>
+Search for the FED3 libary in the Arduino library manager and install the latest version.
+![FED3 library manager](https://github.com/KravitzLabDevices/FED3/blob/main/photos/FED3librarymanager.png)
+
+### 2. Install dependency libraries in your Arduino libraries folder <br>
+Navigate to the FED3 library in your Arduino libraries folder (usually: `Documents\Arduino\libraries\FED3_library\`) and look in the \extras directory for the FED3_support_libraries.zip file. Unzip the contents of this folder.
+
+![FED3 libraries](https://github.com/KravitzLabDevices/FED3/blob/main/photos/FED3_libraries.png)
+
+Copy the extracted libraries to `\Arduino\libraries\`.
+
+
+### 3. Start coding!
+Check out the example FED3 programs by clicking File > Examples > FED3 <br>
+![FED3 examples](https://raw.githubusercontent.com/KravitzLabDevices/FED3/main/photos/FED3example2.png)
+
+
--- OOP/Maze_OOP.py
+++ OOP/Maze_OOP.py
@@ -5,10 +5,7 @@ Created on Wed Oct 28 10:46:18 2020
 @author: Vinicius 
 """
 
-import argparse
-import numpy as np
 import serial
-import time
 import RPi.GPIO as GPIO
 import os
 import pandas as pd
@@ -20,10 +17,6 @@ from Maze import Maze
 from RFID import RFID
 from OpenScale import OpenScale
 
-M = Maze()
-R = RFID()
-S = OpenScale()
-
 # change directory to document data folder
 os.chdir("/home/pi/Documents/data/")
 
@@ -33,9 +26,10 @@ GPIO.setmode(GPIO.BOARD)
 # set pin inputs from arduino
 ard_pi_1 = 35
 ard_pi_2 = 36
-ard_pi_3 = 33
+ard_pi_3 = 37
 ard_pi_4 = 38
-ard_pi_5 = 37
+ard_pi_5 = 33
+clk=12
 
 GPIO.setup(ard_pi_1,GPIO.IN)
 GPIO.setup(ard_pi_2,GPIO.IN)
@@ -43,120 +37,162 @@ GPIO.setup(ard_pi_3,GPIO.IN)
 GPIO.setup(ard_pi_4,GPIO.IN)
 GPIO.setup(ard_pi_5,GPIO.IN)
 
-#set pin inputs from running wheel rotary encoder
-clk=12
-# dt=11
 GPIO.setup(clk,GPIO.IN)
-# GPIO.setup(dt,GPIO.IN)
 clkLastState=GPIO.input(clk)
 
 # set pin outputs to arduino
 Pi_RFID = 16
-Pi_scale = 15
+Pi_exit = 15
+Pi_end = 13
 Pi_capture_1=40
 PiArd_reset=18
+
 GPIO.setup(Pi_RFID,GPIO.OUT)
-GPIO.setup(Pi_scale,GPIO.OUT)
+GPIO.setup(Pi_exit,GPIO.OUT)
+GPIO.setup(Pi_end,GPIO.OUT)
 GPIO.setup(Pi_capture_1,GPIO.OUT)
 GPIO.setup(PiArd_reset,GPIO.OUT)
+
 GPIO.output(Pi_RFID,False)
-GPIO.output(Pi_scale,False)
+GPIO.output(Pi_exit,False)
+GPIO.output(Pi_end,False)
 GPIO.output(Pi_capture_1,False)
+
 GPIO.output(PiArd_reset,False)
 time.sleep(0.3)
 GPIO.output(PiArd_reset,True)
 
 #state variables
-
-choice_flag= False
+beam_break_flag = 0
+task_complete = 0
+choice_flag=False
 counter=0
 cycle=1200 #cycle on running wheel gives approx this many counts
-run_flag=False
-food_flag=False
-animaltag = input("Which animal is in? ")
+run_flag=0
+food_flag=0
+flag_two_animals=False
+previous_animal = []
+current_animal = []
+prevent_entry_flag = False
+flag_animals_left = False
+allow_entry_flag = False
+sequence_index = 0
 
-M_RFID = True
+# instatiating classes
+
+rfid = RFID()
+maze = Maze()
+scale = OpenScale()
+
+mode = 1
 
 while True:
     while True:
-        if M_RFID: 
-            M.log("\nMODE 1\n")
+        if mode == 1: 
+            print("\nMODE 1\n")
+            rfid.read_tag()
+            global animaltag
+            animaltag = rfid.datacheck()
+            prevent_entry_flag = rfid.sequential_check()
+            
+            print(prevent_entry_flag)
+            print(allow_entry_flag)
 
-            ser_string = R.read_tag()
-            M.log(ser_string)
+            if prevent_entry_flag:
+                break
                 
-            if len(ser_string) > 5:
-                M.log('Sending Pi_RFID pulse to Arduino')
-                GPIO.output(Pi_RFID,True) # start signal = high
+            if not prevent_entry_flag:
+                print('Sending Pi_RFID pulse to Arduino')
+                GPIO.output(Pi_RFID,True)
+                
+                print(animaltag)
 
                 #Append data
-                M.append_event("+", "START")
+                maze.append_event("+", "START")
                 
                 # switch mode and clean up RFID
-                M_RFID = False
-                M_scale = True
-            M.log("\nMODE 2\n")
+                mode = 2
+        if mode == 2 and GPIO.input(ard_pi_1): 
+            print("\nMODE 2\n")
+            maze.append_event("+", "Session start")
+            scale.acquire_weight(100)
 
-        if M_scale and GPIO.input(ard_pi_1): 
-            M.log("\nMODE 2\n")
-            M.append_event("+", "Session start")
-            S.acquire_weight()
+            if flag_two_animals == True:
+                GPIO.output(Pi_RFID,False) # open door 1
+                scale.wait_animals_left()
             
-            GPIO.output(Pi_RFID,True) # start signal = high
+            if flag_animals_left:
+                GPIO.output(Pi_end,True) # close door 1
+                time.sleep(10)
+                mode = 1
+                flag_animals_left = False
+                break
+
+            if not flag_two_animals:
+                scale.weight_stats()
+                GPIO.output(Pi_RFID,True) # start signal = high
+                mode = 3
+                choice_flag = True
             
-            M_scale = False
-            M_choice = True
-            M.log("\nMODE 3\n")
+            print("\nMODE 3\n")
                         
-        if M_choice and GPIO.input(ard_pi_2) and not choice_flag: #log a trial start
+        if mode == 3 and GPIO.input(ard_pi_2) and not choice_flag: #log a trial start
             #append run wheel here
-            M.log("\ntrial start\n")
-            M.append_event("*", "BB2")
+            print("\ntrial start\n")
             
-            if food_flag:
-                M.log("appending food pod data")
-                cycles_str = str(counter/cycle)
-                append_event(cycles_str, "Food")
+            # only append BB2 for the first time the animal enters the maze
+            if maze.event_list["Type"] == ["Session start"]:
+                maze.append_event("*", "BB2")
             
-            if run_flag:
-                M.log("appending running wheel data")
-                cycles_str = str(counter/cycle)
-                append_event(cycles_str, "Run")
+            if food_flag==1:
+                print("appending food pod data")
+                cycles_str = round(counter/cycle,4)
+                maze.append_event(cycles_str, "Food_log")
+            
+            if run_flag==1:
+                print("appending running wheel data")
+                cycles_str = round(counter/cycle,4)
+                maze.append_event(cycles_str, "Run_log")
             #start camera capture/opto
             GPIO.output(Pi_capture_1,True)
 
-            M_choice = False
             choice_flag=True
             run_flag=False
             food_flag=False
             counter=0
             
-        if M_choice and GPIO.input(ard_pi_5): #animal going back home
-            M.log("\nMODE 4\n")
-            M.append_event("*", "BB5")
-            GPIO.output(Pi_RFID,False)
-            #stop camera capture/opto
-            GPIO.output(Pi_capture_1,False)
-            time.sleep(15)#so the RFID doesn't read while animal is going back to cage
-            MODE = 1
+        if mode == 3 and GPIO.input(ard_pi_3): #animal going back home
+            flag_animals_left = scale.wait_animals_left()
+            if flag_animals_left:
+                GPIO.output(Pi_RFID,True)
+                GPIO.output(Pi_exit,True)
+                GPIO.output(Pi_capture_1,False)
+                #appending data to database
+                mode = 4
+                flag_animals_left = False
 
-            #appending data to database
-            M.append_event("-", "END")
-            
-            #BREAK OUT OF THE INFINITE LOOP 
+        if mode == 4 and GPIO.input(ard_pi_1):
+            maze.append_event("-", "END")
+            mode = 1
+            GPIO.output(Pi_RFID,False)
+            GPIO.output(Pi_exit,False)
+            GPIO.output(Pi_end,False)
+            GPIO.output(Pi_capture_1,False)
+            #sequence_index = (sequence_index + 1) % 2 # works for two animals
+            sequence_index = (sequence_index + 1) % 3 # works for three animals
             break
                     
-        if M_choice and GPIO.input(ard_pi_4) and choice_flag: #log food pod
-            M.append_event("*", "BB4")
-            M.log("\nfood pod\n")
+        if mode == 3 and GPIO.input(ard_pi_4) and choice_flag: #log food pod
+            maze.append_event("*", "BB4")
+            print("\nfood pod\n")
             
             choice_flag=False
             food_flag=True
             limit=cycle
 
-        if M_choice and GPIO.input(ard_pi_3) and choice_flag: #log a maze event running wheel
-            M.log("\nrunning wheel\n")
-            M.append_event("*", "BB3")
+        if mode == 3 and GPIO.input(ard_pi_3) and choice_flag: #log a maze event running wheel
+            print("\nrunning wheel\n")
+            maze.append_event("*", "BB3")
             
             choice_flag=False
             run_flag=True
@@ -168,7 +204,9 @@ while True:
                 counter += 1  
                 clkLastState = clkState
             if counter >= limit:
-                M.log(counter)    
+                print(counter)    
                 limit=counter+cycle                 
 
-    M.log("finally")
+    allow_entry_flag = False
+    prevent_entry_flag = False
+    print("finally")
